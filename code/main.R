0e98eb28bec1a815d03244e33bab9f14deeec5c8
EdB-DHSC <edward.beake@dhsc.gov.uk>
2021-08-09 17:05:05 +0100

    init

---
+++ code/main.R
@@ -0,0 +1,19 @@
+library(haven)
+library(tidyverse)
+
+#########################
+# SEC 1 - READ IN #
+########################
+
+hse07 <- read_sav("data/hse07ah.sav")
+
+hse17 <- read_sav("data/hse17i_eul_v1.sav")
+
+#############################
+# SEC 2 - DATA WRANGELLING #
+############################
+
+#############################
+# SEC 3 - ANALYSIS #
+####################
+
